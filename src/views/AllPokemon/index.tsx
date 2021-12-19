02bc1507277e2e27d9cd15ad187b8bf22499448c
Caleb <clutchcaleb@gmail.com>
2021-12-19 16:29:23 -0500

    pokemon names and images

--- src/views/AllPokemon/index.tsx
+++ src/views/AllPokemon/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PokemonHolder from "../../components/PokemonHolder";
 import "./AllPokemon.css";
 
@@ -9,6 +9,9 @@ interface Pokemon {
   details: string;
   image: string;
 }
+interface PokemonApi {
+  name: string;
+}
 
 const poke: Pokemon = {
   name: "PokemonName",
@@ -17,19 +20,39 @@ const poke: Pokemon = {
   image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
 };
 
+const dataFormatter = (obj: PokemonApi, idx: number = 1) => {
+  return {
+    name: obj.name,
+    details:
+      "Est excepteur do eiusmod officia consectetur velit veniam ad irure nulla consequat. Aute et ullamco ea laboris ea elit veniam esse. Culpa nulla Lorem ut est ullamco ut exercitation et occaecat sint. Mollit eu ipsum anim non ad proident incididunt incididunt exercitation cupidatat culpa laboris non. Esse sunt amet et Lorem eu est consectetur ea eiusmod ad ipsum ut laborum in.",
+    image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${idx}.png`,
+  };
+};
+
 const AllPokemon = () => {
-  const [pokemon, setPokemon] = useState([poke, poke, poke, poke, poke, poke, poke, poke, poke, poke, poke, poke, poke, poke, poke, poke, poke, poke, poke, poke])
+  const [pokemon, setPokemon] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=25")
+      .then((res) => res.json())
+      .then((data) => setPokemon(data.results.map((elem: PokemonApi, idx: number) => dataFormatter(elem, idx + 1))));
+    }, []);
 
   return (
     <div className="allPokemonPage">
       <h1 className="allPokemonPage__title">This is the all pokemon page</h1>
       <div className="allPokemonPage__grid">
-        { pokemon.map((el, idx) => (
+        {pokemon.map((el: Pokemon, idx) => (
           <div key={idx}>
-            <PokemonHolder name={el.name} details={el.details} image={el.image} count={idx} />
+            <PokemonHolder
+              name={el.name}
+              details={el.details}
+              image={el.image}
+              count={idx}
+            />
           </div>
-        ))
-        }
+        ))}
       </div>
     </div>
   );
