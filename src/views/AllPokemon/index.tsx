2ed2f3160edc46f88b17d21ff29ed26b48194963
Caleb <clutchcaleb@gmail.com>
2021-12-19 21:47:42 -0500

    useRefs

--- src/views/AllPokemon/index.tsx
+++ src/views/AllPokemon/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import PokemonHolder from "../../components/PokemonHolder";
 import { fetchPokemon } from "../../utils/api";
 import "./AllPokemon.css";
@@ -10,34 +10,27 @@ interface Pokemon {
   details: string;
   image: string;
 }
-interface PokemonApi {
-  name: string;
-}
-
-const poke: Pokemon = {
-  name: "PokemonName",
-  details:
-    "Est excepteur do eiusmod officia consectetur velit veniam ad irure nulla consequat. Aute et ullamco ea laboris ea elit veniam esse. Culpa nulla Lorem ut est ullamco ut exercitation et occaecat sint. Mollit eu ipsum anim non ad proident incididunt incididunt exercitation cupidatat culpa laboris non. Esse sunt amet et Lorem eu est consectetur ea eiusmod ad ipsum ut laborum in.",
-  image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
-};
-
-const dataFormatter = (obj: PokemonApi, idx: number = 1) => {
-  return {
-    name: obj.name,
-    details:
-      "Est excepteur do eiusmod officia consectetur velit veniam ad irure nulla consequat. Aute et ullamco ea laboris ea elit veniam esse. Culpa nulla Lorem ut est ullamco ut exercitation et occaecat sint. Mollit eu ipsum anim non ad proident incididunt incididunt exercitation cupidatat culpa laboris non. Esse sunt amet et Lorem eu est consectetur ea eiusmod ad ipsum ut laborum in.",
-    image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${idx}.png`,
-  };
-};
 
 const AllPokemon = () => {
   const [pokemon, setPokemon] = useState([]);
   const [limit, setLimit] = useState(25);
+  const grid = useRef<any>(null);
+  const top = useRef<any>(null);
+
+  const scrollTop = () => {
+    top.current.scrollIntoView();
+  }
+
+  const scrollBottom = () => {
+    grid.current.lastChild.scrollIntoView();
+  }
 
   const grabPokemon = async() => {
     const data = await fetchPokemon(limit);
     setLimit(limit => limit + 25)
     setPokemon(data);
+
+    setTimeout(scrollBottom, 2000)
   }
 
   useEffect(() => {
@@ -46,8 +39,9 @@ const AllPokemon = () => {
 
   return (
     <div className="allPokemonPage">
-      <h1 className="allPokemonPage__title">This is the all pokemon page</h1>
-      <div className="allPokemonPage__grid">
+      <h1 ref={top} className="allPokemonPage__title">all pokemon page</h1>
+      <button className="allPokemonPage__button" onClick={scrollBottom}>Scroll to bottom</button>
+      <div className="allPokemonPage__grid" ref={grid}>
         {pokemon.map((el: Pokemon, idx) => (
           <div key={idx}>
             <PokemonHolder
@@ -59,8 +53,11 @@ const AllPokemon = () => {
           </div>
         ))}
       </div>
-      <button className="allPokemonPage__morePokemon" onClick={() => grabPokemon()}>
+      <button className="allPokemonPage__button" onClick={() => grabPokemon()}>
         More Pokemon
+      </button>
+      <button className="allPokemonPage__button" onClick={scrollTop}>
+          Scroll to top
       </button>
     </div>
   );
